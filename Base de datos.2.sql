187c7a77bc63a18afd87d380b7528ef26c039db7
Zack72-4 <daniedone6@gmail.com>
2026-02-03 22:48:00 -0400

    Add files via upload

---
+++ Base de datos.2.sql
@@ -0,0 +1,35 @@
+
+CREATE DATABASE IF NOT EXISTS tienda_celulares;
+USE tienda_celulares;
+
+-- 2. Tabla de Usuarios
+CREATE TABLE usuarios (
+    id_usuarios INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_completo VARCHAR(100) NOT NULL,
+    nombre_usuario VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    rol ENUM('Admin') NOT NULL,
+    estado TINYINT(1) DEFAULT 1,
+    ultimo_login DATETIME
+);
+CREATE TABLE inventario_celulares (
+    id_celulares INT AUTO_INCREMENT PRIMARY KEY,
+    marca VARCHAR(50) NOT NULL,
+    modelo VARCHAR(50) NOT NULL,
+    imei VARCHAR(20) NOT NULL UNIQUE,
+    color VARCHAR(30),
+    precio_venta DECIMAL(10, 2) NOT NULL,
+    estado_stock ENUM('Disponible', 'Vendido', 'Garantia') DEFAULT 'Disponible'
+);
+
+CREATE TABLE ventas (
+    id_venta INT AUTO_INCREMENT PRIMARY KEY,
+    id_celulares INT NOT NULL,
+    id_usuarios INT NOT NULL,
+    nombre_cliente VARCHAR(100),
+    precio_final DECIMAL(10, 2),
+    fecha_venta TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+    FOREIGN KEY (id_usuarios) REFERENCES usuarios(id_usuarios),
+    FOREIGN KEY (id_celulares) REFERENCES inventario_celulares(id_celulares)
+);
